db99ef9d5a189507260e1e380903fcd2b48184a4
borges <eldevborges@gmail.com>
2026-05-21 21:45:18 -0300

    começando windowsBuilde no projeto empresa-crud

---
+++ empresa_crud/Main.java
@@ -0,0 +1,23 @@
+package view;
+
+import controller.ClientController;
+import model.Cliente;
+import model.DAO.ClienteDAO;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String [] args) {
+       
+    	TelaLogin tl = new TelaLogin ();
+    	
+    	tl.setVisible(true);
+    	
+    	
+
+
+
+}
+}
--- empresa_crud/src/connection/ConexaoBD.java
+++ empresa_crud/src/connection/ConexaoBD.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD {
-    private static final String URL = "jdbc:mysql://localhost:3307/empresa";
+    private static final String URL = "jdbc:mysql://localhost:3306/empresa";
     private static final String USER = "root";
-    private static final String PASSWORD = "alunolab";
+    private static final String PASSWORD = "Lf19092019f-";
 
     public Connection getConnection() {
         try {
